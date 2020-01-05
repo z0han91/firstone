6ec676ceb20f3ae7bb483ae960232f460e37449e
z0han91 <andreiyzakharov@gmail.com>
2020-01-05 14:26:34 +0200

    add base tamplate in base.html, add extend tamplate in index.html. render data on site from db

--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-# from django.http import HttpResponse
+from .models import Post
 
 # Create your views here.
 
 
 def posts_list(request):
-    return render(request, 'blog/index.html')
+    posts = Post.objects.all()
+    return render(request, 'blog/index.html', context={'posts': posts})
